4f04e578f290197a3ae5ad821c6775dfd59c9945
dmail <dmail@google.com>
2025-02-25 15:01:12 -0800

    Updates comment

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -66,7 +66,7 @@ println ("a total of ${jumboCounter} jumbos.")
          totalSize += eachFile.length()
          if (totalSize > 500000) {
            println("500k limit exceeded. ${directories[directoryIndex]} truncated.")
-//           break
+//           break <-- uncomment to make the tool enforce NotebookLM's 500k limit
          }
          eachFile.forEachLine { aggregatedText.append(it).append("\n")  }
        }
